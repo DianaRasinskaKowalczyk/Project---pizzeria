a0b3b2bf286b2228c92f842f44f179a59d088aba
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-04-27 12:38:33 +0200

    Add images js

--- src/js/script.js
+++ src/js/script.js
@@ -94,6 +94,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -105,25 +106,25 @@
       /* DONE START: add eventListener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function(event){
 
-          /* DONE prevent default action for event */
-          event.preventDefault();
+        /* DONE prevent default action for event */
+        event.preventDefault();
 
-            /* DONE find active product (product that has active class) */
-          const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        /* DONE find active product (product that has active class) */
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
          
 
-          /* DONE START a loop for every active product */
-          for(let activeProduct of activeProducts){
+        /* DONE START a loop for every active product */
+        for(let activeProduct of activeProducts){
 
           /* DONE if there is active product and it's not thisProduct.element, remove class active from it */
           if(activeProduct !== thisProduct.element){
             activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
           }
           /* DONE END a loop for every active product */
-          }
+        }
 
-          /* DONE toggle active class on thisProduct.element */
-          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        /* DONE toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
 
@@ -172,7 +173,9 @@
           console.log(optionId, option);
 
           /* check if there is param with the name of paramId in formData and if it includes optionId */
-          if(formData[paramId] && formData[paramId].includes(optionId)){
+
+          const optionSelected = (formData[paramId] && formData[paramId].includes(optionId));
+          if(optionSelected){
 
             /* check if this option is not default*/
             if(option.default !== true){
@@ -182,13 +185,31 @@
             }
           } else {
 
-              /* check if the option is default */
-              if(option.default == true){
+            /* check if the option is default */
+            if(option.default == true){
 
-                /* reduce price variable */
-                price = price - option.price;
-              }
+              /* reduce price variable */
+              price = price - option.price;
             }
+          }
+          /* find image with class paramId-optionId */
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
+          /* check if it's there because not every product has an image */
+          if(optionImage){
+          
+            /* if it's there, check if the option is selected */
+            if(optionSelected){
+
+              /* if the option is selected add class active */
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+              
+            } else{
+
+              /* if it's not selected, remove class active */
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
 
         }
       }
