cdc8f56037148e3d6e1bccf6a39b5e87f542254e
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-05-27 11:04:56 +0200

    Fix eslint problems

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,75 +1,75 @@
-import {select, settings} from './settings.js';
+import {select, settings} from '../settings.js';
 
 
 class AmountWidget{
-    constructor(element){
-      const thisWidget = this;
+  constructor(element){
+    const thisWidget = this;
 
-      // console.log('AmountWidget:', thisWidget);
-      // console.log('constructor arguments:', element);
+    // console.log('AmountWidget:', thisWidget);
+    // console.log('constructor arguments:', element);
 
-      thisWidget.getElements(element);
-      thisWidget.setValue(thisWidget.input.value);
-      thisWidget.initActions();
-    }
+    thisWidget.getElements(element);
+    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.initActions();
+  }
 
-    getElements(element){
-      const thisWidget = this;
+  getElements(element){
+    const thisWidget = this;
 
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    }
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+  }
 
-    announce(){
-      const thisWidget = this;
+  announce(){
+    const thisWidget = this;
 
-      const event = new CustomEvent('updated', {
-        bubbles: true
-      });
-      thisWidget.element.dispatchEvent(event);
-    }
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
 
-    setValue(value){
-      const thisWidget= this;
+  setValue(value){
+    const thisWidget= this;
 
-      const newValue = parseInt(value);
+    const newValue = parseInt(value);
 
-      thisWidget.value = settings.amountWidget.defaultValue;
+    thisWidget.value = settings.amountWidget.defaultValue;
 
-      /* DONE Add validation */
-      if(newValue !== thisWidget.value && 
+    /* DONE Add validation */
+    if(newValue !== thisWidget.value && 
         !isNaN(newValue) && 
         newValue >= settings.amountWidget.defaultMin && 
         newValue <= settings.amountWidget.defaultMax){
-        thisWidget.value = newValue;
-      }
+      thisWidget.value = newValue;
+    }
 
       
-      thisWidget.input.value = thisWidget.value;
+    thisWidget.input.value = thisWidget.value;
 
-      thisWidget.announce();
-    }
-
-    initActions(){
-      const thisWidget = this;
-
-      thisWidget.input.addEventListener('change', function(){
-        thisWidget.setValue(thisWidget.input.value);
-      });
-
-      thisWidget.linkDecrease.addEventListener('click', function(event){
-        event.preventDefault();
-        thisWidget.setValue(--thisWidget.input.value);
-      });
-
-      thisWidget.linkIncrease.addEventListener('click', function(event){
-        event.preventDefault();
-        thisWidget.setValue(++thisWidget.input.value);
-      });
-    }
-   
+    thisWidget.announce();
   }
 
-  export default AmountWidget;
+  initActions(){
+    const thisWidget = this;
+
+    thisWidget.input.addEventListener('change', function(){
+      thisWidget.setValue(thisWidget.input.value);
+    });
+
+    thisWidget.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(--thisWidget.input.value);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(++thisWidget.input.value);
+    });
+  }
+   
+}
+
+export default AmountWidget;
